2df46c2e8c65c58ccb4e270bd2363f360a1f6d35
Eric Undersander <eundersander@gmail.com>
2021-06-07 09:38:10 -0700

    fix for build script

--- build_and_install_habitat_sim_js.sh
+++ build_and_install_habitat_sim_js.sh
@@ -1,6 +1,6 @@
 git submodule update --init
 cd habitat-sim
-build_js.sh --no-web-apps
+./build_js.sh --no-web-apps
 cd ../
 cp habitat-sim/build_js/esp/bindings_js/hsim_bindings.js task/server_files/habitat_vr_app/lib/habitat-sim-js/
 cp habitat-sim/build_js/esp/bindings_js/hsim_bindings.wasm task/server_files/habitat_vr_app/lib/habitat-sim-js/
